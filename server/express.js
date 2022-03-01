29b0df2265884b69f18438ccdef70c7ad2165097
L00P3R93 <vincentkioko51@gmail.com>
2022-03-01 17:30:57 +0300

    Initial Commit

---
+++ server/express.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import path from 'path'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+
+import Template from './../template'
+
+import userRoutes from './routes/user.routes'
+
+import devBundle from './devBundle'
+
+const CURRENT_WORKING_DIR = process.cwd()
+const app = express()
+
+devBundle.compile(app)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieParser())
+app.use(compress())
+app.use(helmet())
+app.use(cors())
+
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+
+app.use('/', userRoutes)
+
+app.get('/', (req, res) => {
+    res.status(200).send(Template())
+})
+
+
+export default app
